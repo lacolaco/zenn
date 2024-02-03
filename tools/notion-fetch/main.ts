d396a767b7684422b38e6cd4c2406667024daa44
Suguru Inatomi <suguru.inatomi@gmail.com>
2024-02-04 00:58:25 +0900

    chore: use notion-fetch

--- tools/notion-fetch/main.ts
+++ tools/notion-fetch/main.ts
@@ -1,5 +1,5 @@
+import { fetchZennPostPages } from '@lacolaco/notion-fetch';
 import { parseArgs } from 'node:util';
-import { NotionDatabase } from './notion';
 import { LocalPostFactory } from './posts/factory';
 import { ImagesRepository, LocalPostsRepository } from './posts/repository';
 
@@ -7,6 +7,8 @@ if (process.env.NOTION_AUTH_TOKEN == null) {
   console.error('Please set NOTION_AUTH_TOKEN');
   process.exit(1);
 }
+
+const { NOTION_AUTH_TOKEN } = process.env;
 
 const postsDir = new URL('../../articles', import.meta.url).pathname;
 const imagesDir = new URL('../../images', import.meta.url).pathname;
@@ -30,21 +32,25 @@ const { values } = parseArgs({
 const { force = false, 'dry-run': dryRun = false, verbose = false } = values;
 
 async function main() {
-  const db = new NotionDatabase();
   const localPostsRepo = new LocalPostsRepository(postsDir, { dryRun });
   const imagesRepo = new ImagesRepository(imagesDir, { dryRun });
   const postFactory = new LocalPostFactory(localPostsRepo, imagesRepo, { forceUpdate: force });
 
   // collect posts from notion
   console.log('Fetching pages...');
-  const pages = await db.queryBlogPages();
+  const pages = await fetchZennPostPages(NOTION_AUTH_TOKEN, dryRun);
   console.log(`Fetched ${pages.length} pages`);
-  if (pages.length > 0) {
-    console.log('Creating posts...');
-    if (verbose) {
-      console.log(JSON.stringify(pages, null, 2));
-    }
-    await postFactory.create(pages);
+  const pagesToUpdate = pages.filter((page) => force || page.changed);
+
+  if (pagesToUpdate.length === 0) {
+    console.log('No pages to update');
+  } else {
+    console.log(`Updating ${pagesToUpdate.length} pages...`);
+    await Promise.all(
+      pagesToUpdate.map(async (page) => {
+        return { ...page, slug: page.properties.slug.rich_text[0].plain_text };
+      }),
+    ).then((pages) => postFactory.create(pages));
   }
   console.log('Done');
 }
